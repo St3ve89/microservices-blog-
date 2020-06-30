0b2a47b005d26863c1edae8607372055f311b2cd
Istvan Acs <istvan.acs89@gmail.com>
2020-06-30 18:23:51 +0100

    Displaying comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default function PostList() {
   const [posts, setPosts] = useState({});
@@ -25,6 +26,7 @@ export default function PostList() {
       >
         <div className='card-body'>
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>

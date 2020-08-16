18c9418a8e657f48b6de9955b299db83ba4f9b5c
Istvan Acs <istvan.acs89@gmail.com>
2020-08-16 13:34:15 +0100

    Change files to meet the requirements for deployin the client side

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ export default function PostCreate() {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post('http://localhost:4000/posts', {
+    await axios.post('http://posts.com/posts', {
       title,
     });
 
